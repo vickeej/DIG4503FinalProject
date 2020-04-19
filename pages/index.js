221006644c15079de0cd2546d510ee6ccb3ee952
Vickee Johnson <vickeejohnson@knights.ucf.edu>
2020-04-19 00:08:11 -0400

    Adjusting for screen sizes

--- pages/index.js
+++ pages/index.js
@@ -8,14 +8,16 @@ import styles from '../components/Style.module.css';
 const Home = () => {
     return(
       <body className = {styles.body}>
-        <Header/>
-        <div className = {styles.container}>
-          <NameSearch/>
-          <IdSearch/>
-          <TypeSearch/>
-          <div id = "reportingArea" className = {styles.reportingArea}></div>
+        <div className = {styles.pokedex}>
+          <Header/>
+          <div className = {styles.container}>
+            <NameSearch/>
+            <IdSearch/>
+            <TypeSearch/>
+            <div id = "reportingArea" className = {styles.reportingArea}></div>
+          </div>
+          <Footer/>
         </div>
-        <Footer/>
       </body>
     );
   }
